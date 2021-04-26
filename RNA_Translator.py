118a0a6290649d3d33d0ec57a785c011aca26cce
benfield97 <fieldben97@gmail.com>
2021-04-26 20:31:36 +1000

    Add files via upload

---
+++ RNA_Translator.py
@@ -0,0 +1,46 @@
+dna = """ATGTACTCATTCGTTTCGGAAGAGACAGGTACGTTAATAGTTAATAGCGTACTTCTTTTTCTTGCTTTCG
+TGGTATTCTTGCTAGTTACACTAGCCATCCTTACTGCGCTTCGATTGTGTGCGTACTGCTGCAATATTGT
+TAACGTGAGTCTTGTAAAACCTTCTTTTTACGTTTACTCTCGTGTTAAAAATCTGAATTCTTCTAGAGTT
+CCTGATCTTCTGGTCTAA"""
+
+RNA_Codons = {
+    # 'M' - START, '*' - STOP
+    "GCU": "A", "GCC": "A", "GCA": "A", "GCG": "A",
+    "UGU": "C", "UGC": "C",
+    "GAU": "D", "GAC": "D",
+    "GAA": "E", "GAG": "E",
+    "UUU": "F", "UUC": "F",
+    "GGU": "G", "GGC": "G", "GGA": "G", "GGG": "G",
+    "CAU": "H", "CAC": "H",
+    "AUA": "I", "AUU": "I", "AUC": "I",
+    "AAA": "K", "AAG": "K",
+    "UUA": "L", "UUG": "L", "CUU": "L", "CUC": "L", "CUA": "L", "CUG": "L",
+    "AUG": "M",
+    "AAU": "N", "AAC": "N",
+    "CCU": "P", "CCC": "P", "CCA": "P", "CCG": "P",
+    "CAA": "Q", "CAG": "Q",
+    "CGU": "R", "CGC": "R", "CGA": "R", "CGG": "R", "AGA": "R", "AGG": "R",
+    "UCU": "S", "UCC": "S", "UCA": "S", "UCG": "S", "AGU": "S", "AGC": "S",
+    "ACU": "T", "ACC": "T", "ACA": "T", "ACG": "T",
+    "GUU": "V", "GUC": "V", "GUA": "V", "GUG": "V",
+    "UGG": "W",
+    "UAU": "Y", "UAC": "Y",
+    "UAA": "*", "UAG": "*", "UGA": "*"
+}
+
+protein = ""
+
+dna = dna.replace("\n", "")
+
+rna_map = dna.maketrans('ATGC', 'UACG')
+
+rna = dna.translate(rna_map)
+
+for i in range(0, len(rna), 3):
+    codon = rna[i: i + 3]
+    protein += RNA_Codons[codon]
+
+
+print(f"The DNA sequence is \n\n{dna}\n")
+print(f"The RNA sequence is \n\n{rna}\n")
+print(f'The protein sequence is \n\n{protein}\n')
